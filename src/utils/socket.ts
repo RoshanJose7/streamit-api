8c4d1d9bdacb188c9f263de35033ac8f843b6a7e
RoshanJose <roshanstark007@gmail.com>
2022-10-10 19:28:40 +0530

    Fix: room temp data cleanup

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,9 @@
+import { join } from "path";
+import { readdir } from "fs";
+import { rm } from "fs/promises";
 import { Server } from "socket.io";
 
+const TEMP_DIR = join(__dirname, "..", "..", "temp");
 const usersInRoom = new Map<string, string[]>();
 const io = new Server({
   path: "/socket.io",
@@ -23,8 +27,24 @@ io.on("connection", (socket) => {
 
   socket.on("leave_room", (data) => {
     if (!usersInRoom.has(data.room)) usersInRoom.delete(data.room);
-
     socket.leave(data.room);
+
+    const users = usersInRoom.get(data.room);
+
+    if (users.length === 0) {
+      const room_dir = join(TEMP_DIR, data.room);
+
+      readdir(room_dir, async (err, files) => {
+        if (err) console.error(err);
+        else {
+          for (let i = 0; i < files.length; i++) {
+            const file_path = join(room_dir, files[i]);
+            await rm(file_path);
+          }
+        }
+      });
+    }
+
     io.in(data.room).emit("notification", {
       type: "log",
       notification: `${data.name} left ${data.room}`,
